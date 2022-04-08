238f48cc001d0a7f0e1a949069e8d364de5addef
leogcia <leon.gonzalez.garcia@gmail.com>
2022-04-08 15:54:36 -0500

    Form nuevos productos, edicion y listado, jsonSEver

--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -1,6 +1,41 @@
 function NuevoProducto() {
     return (
-        <h1>Desde nuevo producto</h1>
+        <div className="row justify-content-center">
+            <div className="cl-md-8">
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="text-center mb-4 font-weight-bold">Agregar nuevo Producto</h2>
+
+                        <form>
+                            <div className="form-group">
+                                <label htmlFor="nombre">Nombre Producto</label>
+                                <input
+                                    id="nombre"
+                                    type='text'
+                                    className="form-control"
+                                    placeholder="Nombre del Producto."
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="precio">Precio Producto</label>
+                                <input
+                                    id="precio"
+                                    type='number'
+                                    className="form-control"
+                                    placeholder="Precio del Producto."
+                                />
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+                            >Agregar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
